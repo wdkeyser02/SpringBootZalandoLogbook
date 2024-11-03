40effbc06d0ed23bf871a9bfa8838ff1920667e9
willydekeyser <ma074172@skynet.be>
2024-11-03 10:29:17 +0100

    Setup
    Signed-off-by: willydekeyser <ma074172@skynet.be>

---
+++ Zalando_Logbook_1/src/main/java/willydekeyser/ZalandoLogbook1Application.java
@@ -0,0 +1,13 @@
+package willydekeyser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZalandoLogbook1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZalandoLogbook1Application.class, args);
+	}
+
+}
