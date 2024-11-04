dd035a04a9bb0d2756fb0b6717f1d496c840c169
willydekeyser <ma074172@skynet.be>
2024-11-04 17:11:07 +0100

    Update Logbook

--- Zalando_Logbook_Config_1/src/main/java/willydekeyser/config/LogBookConfig.java
+++ Zalando_Logbook_Config_1/src/main/java/willydekeyser/config/LogBookConfig.java
@@ -5,14 +5,24 @@ import static org.zalando.logbook.core.QueryFilters.defaultValue;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.zalando.logbook.Logbook;
+// import org.zalando.logbook.core.CurlHttpLogFormatter;
+import org.zalando.logbook.core.DefaultHttpLogFormatter;
+import org.zalando.logbook.core.DefaultSink;
+import org.zalando.logbook.core.StreamHttpLogWriter;
+// import org.zalando.logbook.json.JsonHttpLogFormatter;
 
 @Configuration
 public class LogBookConfig {
 	
+	DefaultSink httpSink = new DefaultSink(new DefaultHttpLogFormatter(), new StreamHttpLogWriter()); 
+    // DefaultSink curlSink = new DefaultSink(new CurlHttpLogFormatter(), new DefaultHttpLogWriter());
+    // DefaultSink jsonSink = new DefaultSink(new JsonHttpLogFormatter(), new DefaultHttpLogWriter());
+    
 	@Bean
 	Logbook logbook() {
 		return Logbook.builder()
 				.queryFilter(defaultValue())
+				.sink(httpSink)
 				.build();
 	}
 
