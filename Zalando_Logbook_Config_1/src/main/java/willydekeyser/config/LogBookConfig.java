d03a1b92c36079f904ae11002254c7b89f975d6a
willydekeyser <ma074172@skynet.be>
2024-11-05 09:03:38 +0100

    Add DefaultSink
    Signed-off-by: willydekeyser <ma074172@skynet.be>

--- Zalando_Logbook_Config_1/src/main/java/willydekeyser/config/LogBookConfig.java
+++ Zalando_Logbook_Config_1/src/main/java/willydekeyser/config/LogBookConfig.java
@@ -3,6 +3,9 @@ package willydekeyser.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.zalando.logbook.Logbook;
+import org.zalando.logbook.core.DefaultHttpLogFormatter;
+import org.zalando.logbook.core.DefaultHttpLogWriter;
+import org.zalando.logbook.core.DefaultSink;
 
 @Configuration
 public class LogBookConfig {
@@ -10,6 +13,7 @@ public class LogBookConfig {
 	@Bean
 	Logbook logbook() {
 		return Logbook.builder()
+				.sink(new DefaultSink(new DefaultHttpLogFormatter(), new DefaultHttpLogWriter()))
 				.build();
 	}
 
