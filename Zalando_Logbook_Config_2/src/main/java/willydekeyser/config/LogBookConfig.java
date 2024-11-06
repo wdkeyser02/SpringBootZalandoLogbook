7a03b5140a9ac11ee1c1e492048a901d07cfc92b
willydekeyser <ma074172@skynet.be>
2024-11-06 16:26:16 +0100

    Update config

--- Zalando_Logbook_Config_2/src/main/java/willydekeyser/config/LogBookConfig.java
+++ Zalando_Logbook_Config_2/src/main/java/willydekeyser/config/LogBookConfig.java
@@ -1,14 +1,13 @@
 package willydekeyser.config;
 
 import static org.zalando.logbook.core.QueryFilters.replaceQuery;
-import static org.zalando.logbook.json.JsonPathBodyFilters.jsonPath;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.zalando.logbook.Logbook;
+import org.zalando.logbook.core.DefaultHttpLogFormatter;
 import org.zalando.logbook.core.DefaultHttpLogWriter;
 import org.zalando.logbook.core.DefaultSink;
-import org.zalando.logbook.json.JsonHttpLogFormatter;
 
 @Configuration
 public class LogBookConfig {
@@ -16,20 +15,20 @@ public class LogBookConfig {
 	@Bean
 	Logbook logbook() {
 		return Logbook.builder()
-				.correlationId(new CustomCorrelationId())
-				.attributeExtractor(new OriginExtractor())
+				//.correlationId(new CustomCorrelationId())
+				//.attributeExtractor(new OriginExtractor())
 				//.condition(new CustomCondition())
 			    //.queryFilter(new CustomQueryFilter())
-				.queryFilter(replaceQuery("$.password", "TESTEN"))
+				.queryFilter(replaceQuery("password", "TESTEN"))
 			    //.pathFilter(new CustomPathFilter())
 			    //.headerFilter(new CustomHeaderFilter())
 			    //.bodyFilter(new CustomBodyFilter())
-				.bodyFilter(jsonPath("$.password").replace("<XXXXXXX>"))
+				//.bodyFilter(jsonPath("$.password").replace("<XXXXXXX>"))
 			    //.requestFilter(new CustomRequestFilter())
 			    //.responseFilter(new CustomResponseFilter())
-			    .strategy(new CustomStrategy())
+			    //.strategy(new CustomStrategy())
 				.sink(new DefaultSink(
-						new JsonHttpLogFormatter(), 
+						new DefaultHttpLogFormatter(), 
 						new DefaultHttpLogWriter()
 				))
 				.build();
