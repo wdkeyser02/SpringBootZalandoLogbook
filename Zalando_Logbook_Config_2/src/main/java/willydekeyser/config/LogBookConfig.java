c22c7b5f89c923cbddeb5e6162363ba32f8a8ece
willydekeyser <ma074172@skynet.be>
2024-11-06 22:22:17 +0100

    Update Config
    Signed-off-by: willydekeyser <ma074172@skynet.be>

--- Zalando_Logbook_Config_2/src/main/java/willydekeyser/config/LogBookConfig.java
+++ Zalando_Logbook_Config_2/src/main/java/willydekeyser/config/LogBookConfig.java
@@ -8,6 +8,11 @@ import org.zalando.logbook.Logbook;
 import org.zalando.logbook.core.DefaultHttpLogFormatter;
 import org.zalando.logbook.core.DefaultHttpLogWriter;
 import org.zalando.logbook.core.DefaultSink;
+import org.zalando.logbook.core.RequestFilters;
+import org.zalando.logbook.core.ResponseFilters;
+
+import static org.zalando.logbook.core.Conditions.contentType;
+import static org.zalando.logbook.core.Conditions.exclude;
 
 @Configuration
 public class LogBookConfig {
@@ -15,6 +20,9 @@ public class LogBookConfig {
 	@Bean
 	Logbook logbook() {
 		return Logbook.builder()
+				.requestFilter(RequestFilters.replaceBody(message -> contentType("text/*", "text/html").test(message) ? "Request false" : "Request TRUE"))
+				.responseFilter(ResponseFilters.replaceBody(message -> contentType("text/*", "text/html").test(message) ? "Request false" : "Response TRUE"))
+				.condition(exclude())
 				//.correlationId(new CustomCorrelationId())
 				//.attributeExtractor(new OriginExtractor())
 				//.condition(new CustomCondition())
